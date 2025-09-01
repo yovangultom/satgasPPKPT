827353067fe0383d5856ed225dfbc475ff783813
yovangultom <yovan.gultom21@gmail.com>
2025-09-01 20:43:39 +0700

    fix route

--- app/Http/Middleware/RedirectIfAdminOrPetugas.php
+++ app/Http/Middleware/RedirectIfAdminOrPetugas.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; // Tambahkan ini
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAdminOrPetugas
@@ -18,9 +18,8 @@ class RedirectIfAdminOrPetugas
     {
         if (Auth::check()) {
             $user = Auth::user();
-            // Jika user adalah admin atau petugas, redirect mereka ke panel admin
-            if ($user->hasAnyRole(['admin', 'petugas'])) {
-                return redirect()->route('filament.admin.pages.dashboard'); // Redirect ke dashboard Filament
+            if ($user->hasAnyRole(['admin', 'petugas', 'rektor', 'htl'])) {
+                return redirect()->route('filament.admin.pages.dashboard');
             }
         }
 
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengaduanPublikController;
 use App\Http\Controllers\PengaduanPdfController;
 use App\Http\Controllers\NotificationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -12,24 +14,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth', 'verified', function (Request $request, Closure $next) {
+    if (Auth::check() && Auth::user()->hasAnyRole(['admin', 'petugas', 'rektor', 'htl'])) {
+        return redirect()->route('filament.admin.pages.dashboard');
+    }
+    return $next($request);
+}])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::get('/lapor', [PengaduanPublikController::class, 'create'])->name('lapor.create');
     Route::post('/lapor', [PengaduanPublikController::class, 'store'])->name('lapor.store');
+
     Route::get('/pengaduan', [PengaduanPublikController::class, 'index'])->name('pengaduan.index');
     Route::get('/pengaduan/{pengaduan}', [PengaduanPublikController::class, 'show'])->name('pengaduan.show');
+
+    Route::post('/pengaduan/{pengaduan}/pesan', [PengaduanPublikController::class, 'storeMessage'])->name('pengaduan.storeMessage');
+});
+
+
+Route::middleware('auth')->group(function () {
     Route::get('/pengaduans/{pengaduan}/pdf', [PengaduanPdfController::class, 'generatePdf'])
         ->name('pengaduan.pdf');
-    Route::post('/pengaduan/{pengaduan}/pesan', [PengaduanPublikController::class, 'storeMessage'])
-        ->middleware('auth')
-        ->name('pengaduan.storeMessage');
     Route::get('/bap/export/{bap}', [PengaduanPdfController::class, 'exportBapPdf'])->name('bap.export.pdf');
     Route::get('/notifications/{id}/read', [NotificationController::class, 'markAsRead'])->name('notification.read');
     Route::get('/borang-penanganan/{borangPenanganan}/export-pdf', [PengaduanPdfController::class, 'exportPenangananPdf'])
