8906f4ab29c00758b057fd5eaef8160916cd9cad
yovangultom <yovan.gultom21@gmail.com>
2025-09-17 15:44:45 +0700

    rev

--- resources/views/filament/pages/manage-ketua-satgas.blade.php
+++ resources/views/filament/pages/manage-ketua-satgas.blade.php
@@ -2,8 +2,16 @@
     <form wire:submit.prevent="submit">
         {{ $this->form }}
 
-        <x-filament::button type="submit" class="mt-4">
-            Simpan
+        <x-filament::button type="submit" class="mt-4" wire:loading.attr="disabled" wire:target="data.tanda_tangan">
+
+            <div wire:loading wire:target="data.tanda_tangan" class="flex items-center">
+                <x-filament::loading-indicator class="h-5 w-5 mr-2" />
+                <span>Mengunggah file...</span>
+            </div>
+
+            <span wire:loading.remove wire:target="data.tanda_tangan">
+                Simpan
+            </span>
         </x-filament::button>
     </form>
 </x-filament::page>
